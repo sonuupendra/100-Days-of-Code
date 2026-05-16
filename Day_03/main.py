09569910703e9faf8c49a0b4f80df8f9552b98f6
sonuupendra <sonuupendrabhat@gmail.com>
2026-05-17 00:31:14 +0530

    Added Day_03

--- Day_03/main.py
+++ Day_03/main.py
@@ -22,10 +22,10 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-direction = input("In which direction do you wanna go? Press R for right and L left?")
+direction = input("In which direction do you wanna go? Press R for right and L left?\n")
 
-if direction == "L" or "l":
-    weapon = input("Please select one: SWORD or RIFLE.")
+if direction.lower() == "l":
+    weapon = input("Please select one: SWORD or RIFLE.\n")
     if weapon.lower() == "sword":
         print('''
               
@@ -43,8 +43,8 @@ if direction == "L" or "l":
                           ||  gnv
                           \/ 
               ''')
-        print("You may use the weapon for rest of your mission. To proceed, please select a location")
-        location = input("Choose a location: Forest or Ocean?")
+        print("You may use the weapon for rest of your mission. To proceed, please select a location\n")
+        location = input("Choose a location: Forest or Ocean?\n")
         if location.lower() == "forest":
             print("You wandered too far in search of treasure… and got lost.")  
         elif location.lower() == "ocean":
@@ -65,12 +65,63 @@ if direction == "L" or "l":
                                   /####/           \##\
                                  '===='             `=`
               ''')
-        print("You may use the weapon for rest of your mission. To proceed, please select a location")
-        location = input("Choose a location: Desert or Arctic Ice?")
+        print("You may use the weapon for rest of your mission. To proceed, please select a location\n")
+        location = input("Choose a location: Desert or Arctic Ice?\n")
         if location.lower() == "desert":
             print("The scorching desert drained your strength. You got lost in the endless sands.")  
         elif location.lower() == "arctic ice":
             print("You crossed the frozen arctic ice and discovered the hidden treasure. You win!")
+        else:
+            print("Provide a correct selection.")
+    
+    else:
+        print("Provide a correct selection.")
+
+if direction.lower() == "r":
+    weapon = input("Please select one: AXE or CROSSBOW.\n")
+    if weapon.lower() == "axe":
+        print('''
+                
+           __
+        ,. |_'.
+       / / /:\ \
+     _/_/_/::: |
+    /o_'/o>::/ /
+    / /'/:::/ /
+   / /_/::.'_/     mrf
+  / / \__.-'
+ / /
+/ /
+ /
+              
+                    
+              ''')
+        print("You may use the weapon for rest of your mission. To proceed, please select a location\n")
+        location = input("Choose a location: Grassland or Antartic Ice?\n")
+        if location.lower() == "grassland":
+            print("The peaceful grasslands guided you safely to the hidden treasure. You win!")  
+        elif location.lower() == "antartic ice":
+            print("You wandered across the Antarctic ice and vanished into the endless snow.")
+        else:
+            print("Provide a correct selection.")
+        
+    elif weapon.lower()== "crossbow":
+        print('''
+                .-.                                   
+               /  \\                                
+          .---/-+--||                               
+          |  K=====++->                             
+          '---\-+--||                               
+               \  //                                 
+                `-'      
+                                    
+              ''')
+        print("You may use the weapon for rest of your mission. To proceed, please select a location\n")
+        location = input("Choose a location: Ancient Building or Cave?\n")
+        if location.lower() == "ancient building":
+            print("You entered the ancient building and became trapped in its endless ruins.")  
+        elif location.lower() == "cave":
+            print("The dark cave led you straight to the legendary treasure. Victory is yours!")
         else:
             print("Provide a correct selection.")
     
